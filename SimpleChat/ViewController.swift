c9437164211e8d08e5bb7398ec62e6ca4b28cc88
Ngyen Quoc Tuan <tuannguyen1702@gmail.com>
2017-02-24 17:59:07 +0700

    Add contact view & chatting view

--- SimpleChat/ViewController.swift
+++ SimpleChat/ViewController.swift
@@ -8,31 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var contactTable: UITableView!
+    
+    let users = ["Nguyen Quoc Tuan", "Nguyen Duy Anh", "Nguyen Van Tin", "Ha Anh Tuan", "Tran Van Tam Em", "Tran Nam Nghien",
+        "Cao Thi Kim Chau"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.contactTable.delegate =  self
+        self.contactTable.dataSource = self
+        self.contactTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "customcell")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func viewDidAppear(animated: Bool) {
+        
+        self.contactTable.reloadData()
+        
         let isLoggedIn = NSUserDefaults.standardUserDefaults().boolForKey("isLoggedIn");
         if(!isLoggedIn){
             self.performSegueWithIdentifier("loginView", sender: self);
         }
         
     }
-
+    
     @IBAction func logoutButtonTapped(sender: AnyObject) {
         NSUserDefaults.standardUserDefaults().setBool(false, forKey: "isLoggedIn");
         NSUserDefaults.standardUserDefaults().synchronize();
         
         self.performSegueWithIdentifier("loginView", sender: self);
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.users.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath)
+        cell.textLabel?.text = users[indexPath.item]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("chatView", sender: self);
     }
 }
 
